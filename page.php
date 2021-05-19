1de12687c843669247e8137dcadf65a1ad6fb1e4
Dingwen Wang <wangdingwen07@gmail.com>
2021-05-19 09:12:10 +0800

    Update

---
+++ page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Redirecting...</title>
+	<link rel="icon" type="images/x-icon" href="../images/favicon.ico">
+</head>
+<body>
+
+<?php
+$ipl_origin = "zhs";
+include "bee447c0.php";
+header("refresh:0;url=https://www.baidu.com");
+echo "Redirecting...";
+?>
+</body>
+</html>
